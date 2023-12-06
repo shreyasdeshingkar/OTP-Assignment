dd46ef015ba822a1774c3d5051243d04a9b73100
Shreyas Deshingkar <119831095+shreyasdeshingkar@users.noreply.github.com>
2023-12-06 11:53:16 +0530

    converted to UML association relation

--- validate_otp.py
+++ validate_otp.py
@@ -1,31 +1,40 @@
 import random
 import smtplib
 from twilio.rest import Client
-import keys  # assuming keys.py contains necessary credentials
+import keys
 
 class OTPManager:
     def __init__(self):
-        self.email_server = smtplib.SMTP('smtp.gmail.com', 587)
-        self.email_server.starttls()
-        self.email_server.login('shreyasdeshingkar@gmail.com', 'muacwkbygeasmtow')
-
-        self.sms_client = Client(keys.account_sid, keys.auth_token)
+        pass
 
     def generate_otp(self):
-        return ''.join([str(random.randint(0, 9)) for _ in range(6)])
+        """
+        + generate_otp(): String
+        """
+        return ''.join([str(random.randint(0, 9)) for i in range(6)])
 
     def send_otp_over_email(self, email, otp):
+        """
+        + send_otp_over_email(email: String, otp: String): void
+        """
         if self.validate_email(email):
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login('shreyasdeshingkar@gmail.com', 'muacwkbygeasmtow')
             msg = f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.' \
                   f'\nThis is a system-generated mail, so do not reply.'
-            self.email_server.sendmail("shreyasdeshingkar@gmail.com", email, msg)
+            server.sendmail("shreyasdeshingkar@gmail.com", email, msg)
             print("OTP is sent to email via email.")
         else:
             print("Invalid email address.")
 
     def send_otp_over_mobile(self, mobile, otp):
+        """
+        + send_otp_over_mobile(mobile: String, otp: String): void
+        """
         if self.validate_mobile(mobile):
-            msg = self.sms_client.messages.create(
+            client = Client(keys.account_sid, keys.auth_token)
+            msg = client.messages.create(
                 body=f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.'
                      f'\nThis is a system-generated message, so do not reply.',
                 from_=keys.twilio_number,
@@ -35,13 +44,18 @@ class OTPManager:
         else:
             print("Invalid mobile number.")
 
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
+        """
+        - validate_email(email: String): bool
+        """
         return "@gmail" in email and "." in email
 
-    @staticmethod
-    def validate_mobile(mobile):
+    def validate_mobile(self, mobile):
+        """
+        - validate_mobile(mobile: String): bool
+        """
         return len(mobile) == 10 and mobile.isdigit()
+
 
 if __name__ == "__main__":
     otp_manager = OTPManager()
