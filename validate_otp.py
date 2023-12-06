4871c5d64c5b613efd6179944d1f497618eb5b4c
Shreyas Deshingkar <119831095+shreyasdeshingkar@users.noreply.github.com>
2023-12-06 10:35:05 +0530

    Using Refactor

--- validate_otp.py
+++ validate_otp.py
@@ -1,16 +1,23 @@
 import random
 import smtplib
 from twilio.rest import Client
+import keys
 
-class OTPGenerator:
-    @staticmethod
-    def generate_otp():
-        return ''.join([str(random.randint(0, 9)) for i in range(6)])
+class OTPManager:
+    def generate_otp(self):
+        return ''.join([str(random.randint(0, 9)) for _ in range(6)])
+
+    def send_otp_over_email(self, email, otp):
+        email_sender = EmailSender()
+        email_sender.send_otp(email, otp)
+
+    def send_otp_over_mobile(self, mobile, otp):
+        mobile_sender = MobileSender()
+        mobile_sender.send_otp(mobile, otp)
 
 class EmailSender:
-    @staticmethod
-    def send_otp(email, otp):
-        if EmailValidator.validate_email(email):
+    def send_otp(self, email, otp):
+        if EmailValidator().validate_email(email):
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
             server.login('shreyasdeshingkar@gmail.com', 'muacwkbygeasmtow')
@@ -22,9 +29,8 @@ class EmailSender:
             print("Invalid email address.")
 
 class MobileSender:
-    @staticmethod
-    def send_otp(mobile, otp):
-        if MobileValidator.validate_mobile(mobile):
+    def send_otp(self, mobile, otp):
+        if MobileValidator().validate_mobile(mobile):
             client = Client(keys.account_sid, keys.auth_token)
             msg = client.messages.create(
                 body=f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.'
@@ -37,20 +43,19 @@ class MobileSender:
             print("Invalid mobile number.")
 
 class EmailValidator:
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
         return "@gmail" in email and "." in email
 
 class MobileValidator:
-    @staticmethod
-    def validate_mobile(mobile):
+    def validate_mobile(self, mobile):
         return len(mobile) == 10 and mobile.isdigit()
 
 if _name_ == "_main_":
-    otp = OTPGenerator.generate_otp()
+    otp_manager = OTPManager()
+    otp = otp_manager.generate_otp()
 
     email = "manisha.deshingkar@gmail.com"
-    EmailSender.send_otp(email, otp)
+    otp_manager.send_otp_over_email(email, otp)
 
     mobile = "8625839939"
-    MobileSender.send_otp(mobile, otp)
+    otp_manager.send_otp_over_mobile(mobile, otp)
